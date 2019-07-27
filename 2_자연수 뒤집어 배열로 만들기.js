a59da461bb350945a50ef2d50d3e575a627c1fe0
Mengkko <aappcc1@icloud.com>
2019-07-27 09:16:07 +0900

    주석 추가 2

--- 2_자연수 뒤집어 배열로 만들기.js
+++ 2_자연수 뒤집어 배열로 만들기.js
@@ -8,7 +8,7 @@
 // 12345	[5,4,3,2,1]
 
 function solution(n) {
-  var answer = (n + "").split("").reverse();
+  const answer = (n + "").split("").reverse();
   // 자바스크립트의 자동 형 변환을 이용해서 자연수에 + ""(문자열)을 더해주면
   // 자동으로 문자열로 변환이 됩니다.
   // split메소드를 통해 배열로 변환한뒤 reverse메소드를 이용해서 역정렬을 해줍니다.
