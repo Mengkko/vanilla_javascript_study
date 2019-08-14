cced62209ec6b8ed83d9a76a491d0adb1d7c50ff
Mengkko <aappcc1@icloud.com>
2019-08-15 00:23:25 +0900

    modi code

--- 과제/7_정수 내림차순으로 배치하기.js
+++ 과제/7_정수 내림차순으로 배치하기.js
@@ -1,14 +1,14 @@
 function solution(n) {
   var answer =
-    (n + "")
-      // n => 문자열
+      (n + "")
       .split("")
-      // 문자열 => 배열
       .sort()
-      // 배열 => 오름차순 정렬
       .reverse()
-      // 역정렬
       .join("") * 1;
+  // n => 문자열
+  // 문자열 => 배열
+  // 배열 => 오름차순 정렬
+  // 역정렬
   // 배열 => 문자열 => 숫자형
   return answer;
 }
